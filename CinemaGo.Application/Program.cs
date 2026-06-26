42bbbde2d0534b5da56cc15e390c40a80d56c78b
Juan Ramon Fernandez Mojica <jafernandez@mardom.com>
2026-06-26 15:58:56 -0400

    Add infrastructure setup with DbContext and dependency injection for PostgreSQL

--- CinemaGo.Application/Program.cs
+++ CinemaGo.Application/Program.cs
@@ -1,4 +1,9 @@
+using CinemaGo.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
